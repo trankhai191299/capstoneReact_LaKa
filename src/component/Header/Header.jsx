37d152c7fb480af39b4242c76480254da97842e8
khaitran <akashikazuo99@gmail.com>
2022-09-17 23:35:13 +0700

    react-router-dom tam on

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,7 +1,52 @@
 import React from 'react'
-
+import { NavLink } from "react-router-dom";
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <div className="header-content">
+        <div className="container d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <NavLink to="/">
+              <img src="/img/image 3.png" alt="..." />
+            </NavLink>
+          </div>
+          <div className="user-section">
+            <NavLink to="/" className="cart d-inline-block">
+              <img src="/img/image 8.png" alt="..." />
+              <span className="amount-item">(1)</span>
+            </NavLink>
+            <span className="button-area">
+              <NavLink to="/login" className="btn ms-2 me-3">
+                Login
+              </NavLink>
+              <NavLink to="/register" className="btn">
+                Register
+              </NavLink>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="navbarShop">
+        <div className="container">
+          <div className="nav">
+            <NavLink className="nav-link active" to="/">
+              Home
+            </NavLink>
+            <a className="nav-link" href="#">
+              Men
+            </a>
+            <a className="nav-link" href="#">
+              Women
+            </a>
+            <a className="nav-link" href="#">
+              Kid
+            </a>
+            <a className="nav-link" href="#">
+              Sport
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register';
+import './assets/scss/styles.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='' element={<App/>}>
+        <Route index element={<Home/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='*' element={<Navigate to={<Home/>}/>}></Route>
+        <Route path='login' element={<Login/>}></Route>
+        <Route path='register' element={<Register/>}></Route>
+        {/* <Route path='profile' element={<Login/>}></Route>
+        <Route path='search' element={<Login/>}></Route>
+        <Route path='cart' element={<Login/>}></Route>
+        <Route path='detail' element={<Login/>}></Route> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
