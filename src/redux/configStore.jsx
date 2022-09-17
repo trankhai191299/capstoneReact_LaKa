97b7217d5f168e150f90ef2c58e63c2051297730
khaitran <akashikazuo99@gmail.com>
2022-09-18 00:12:35 +0700

    chinh lai reducer

--- src/redux/configStore.jsx
+++ src/redux/configStore.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import productReducer from './reducers/productReducer'
 
 export const store = configureStore({
     reducer:{
         number : (state = 1,action)=>{
             return 1
-        }
+        },
+        productReducer:productReducer,
     }
 })
